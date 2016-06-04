64545d7cfe3eb8ea19aeab349de121106e13ef2d
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-06-04 15:53:12 +0300

    #1 - тестовый тест 2

--- division_perm/tests/test.py
+++ division_perm/tests/test.py
@@ -4,5 +4,8 @@ from django.test import TestCase
 
 class TestD(TestCase):
 
-    def test(self):
+    def test_one(self):
+        self.assertEquals(1, 1)
+
+    def test_tow(self):
         self.assertEquals(1, 1)
