99985502f681889cb28f65b7b917018481b9ba72
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-06-20 22:10:49 +0300

    #2 - тесты в процессе + добавление name в urls

--- division_perm/tests/test_employee.py
+++ division_perm/tests/test_employee.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 from .. import consts
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
@@ -20,6 +19,7 @@ class BaseEmployee(TestCase):
 
         role = models.Role.objects.create(name='манеджер', code=func_read.code, level=2, division=self.division)
         self.empl.roles.add(role)
+        self.empl.full_access.add(self.division)
         self.client.login(username=self.user.username, password='123')
 
     def get_url(self):
@@ -83,27 +83,96 @@ class EmployeeCreate(BaseEmployee):
         self.assertEqual(response.status_code, 200)
 
     def test_create(self):
-        p = {
-            'username': u'ivanov',
-            'password1': 't1234567',
-            'password2': 't1234567',
-            'last_name': u'Иванов',
-            'first_name': u'Иван',
-            'middle_name': u'Иванович',
-            # 'divisions': [self.division.id,],
-            'full_access': str(self.division.id),
-            # 'read_access': [self.division.id,],
-            'is_active': True,
-            'can_external': False,
-        }
+        p = self.get_params()
+        response = self.client.post(self.get_url(), p, follow=True)
+        self.assertEqual(len(response.redirect_chain), 1)
+        created_empl = models.Employee.objects.filter(user__username=p['username'])[0]
+        self.assertEqual(response.redirect_chain[0], ('/perm/employee/%s/' % created_empl.id, 302))
+        self.assertEqual(p['last_name'], created_empl.last_name)
+        self.assertEqual(p['first_name'], created_empl.first_name)
+        self.assertEqual(p['middle_name'], created_empl.middle_name)
+        self.assertIn(p['divisions'], created_empl.divisions.all().values_list('id', flat=True))
+        self.assertIn(p['full_access'], created_empl.full_access.all().values_list('id', flat=True))
+        self.assertIn(p['read_access'], created_empl.read_access.all().values_list('id', flat=True))
+        self.assertEqual(p['is_active'], created_empl.user.is_active)
+        self.assertEqual(p['can_external'], created_empl.can_external)
 
-        response = self.client.post(self.get_url(), p)
+
+class EmployeeUpdate(BaseEmployee):
+    view_path = 'perm_employee_update'
+
+    def get_url(self):
+        url = reverse(self.view_path, args=[self.empl.id])
+        return url
+
+    def test_200(self):
+        response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, 200)
-        print (response.context['form'].errors)
-        created_empl = models.Employee.objects.filter(username=p['username'])[0]
+
+    def test_update(self):
+        p = self.get_params()
+        response = self.client.post(self.get_url(), p, follow=True)
+        self.assertEqual(response.status_code, 200)
+        update_empl = models.Employee.objects.get(id=self.empl.id)
+        self.assertEqual(response.redirect_chain[0], ('/perm/employee/%s/' % update_empl.id, 302))
+        self.assertEqual(p['last_name'], update_empl.last_name)
+        self.assertEqual(p['first_name'], update_empl.first_name)
+        self.assertEqual(p['middle_name'], update_empl.middle_name)
+        self.assertIn(p['divisions'], update_empl.divisions.all().values_list('id', flat=True))
+        self.assertIn(p['full_access'], update_empl.full_access.all().values_list('id', flat=True))
+        self.assertIn(p['read_access'], update_empl.read_access.all().values_list('id', flat=True))
+        self.assertEqual(p['is_active'], update_empl.user.is_active)
+        self.assertEqual(p['can_external'], update_empl.can_external)
 
 
-        self.assertEqual(created_empl.last_name, p['last_name'])
-        self.assertEqual(created_empl.first_name, p['first_name'])
-        self.assertEqual(created_empl.middle_name, p['middle_name'])
+class EmployeeDelete(BaseEmployee):
+    view_path = 'perm_employee_delete'
 
+    def get_url(self):
+        url = reverse(self.view_path, args=[self.empl.id])
+        return url
+
+    def test_200(self):
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete(self): # может стоит еще одно пользователя создать
+        response = self.client.post(self.get_url(), follow=True)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(models.Employee.objects.filter(id=self.empl.id).count(), 0)
+
+
+class EmployeePasswordChange(BaseEmployee):
+    view_path = 'perm_employee_password_change'
+
+    def get_url(self):
+        url = reverse(self.view_path, args=[self.empl.id])
+        return url
+
+    def test_200(self):
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_change(self):
+        new_password = u'new_password'
+        old_password = self.empl.user.password
+        p = {
+            'password1': new_password,
+            'password2': new_password,
+        }
+        response = self.client.post(self.get_url(), p, follow=True)
+        self.assertEqual(response.redirect_chain[0], ('..', 302))
+        update_empl = models.Employee.objects.get(id=self.empl.id)
+        self.assertNotEqual(update_empl.user.password, old_password)
+
+
+class EmployeeRoles(BaseEmployee):
+    view_path = 'perm_employee_roles'
+
+    def get_url(self):
+        url = reverse(self.view_path, args=[self.empl.id])
+        return url
+
+    def test_200(self):
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
