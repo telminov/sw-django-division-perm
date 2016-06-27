3b613de55ed5d99c576d8ff6284d2e64937410a5
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-06-27 18:35:20 +0300

    #7 - базовый класс для тестирования списков

--- division_perm/tests/helpers.py
+++ division_perm/tests/helpers.py
@@ -1,12 +1,17 @@
 # coding: utf-8
 
-class ListTestMixin(object):
-    success_url = None
+
+class LoginRequiredTestMixin(object):
 
     def test_is_not_authenticated(self):
         self.client.logout()
         response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 403) # todo: страннова-то логичнее бы было редиректить на страницу логина
+
+
+class ListTestMixin(LoginRequiredTestMixin):
+    success_url = None
+    model_access = None
 
     def test_no_roles(self):
         self.user.employee.roles.all().delete()
@@ -21,18 +26,18 @@ class ListTestMixin(object):
             (self.success_url, 302)
         )
 
-
-
+    def test_empty_object(self):
+        for obj in self.model_access.objects.all():
+            obj.full_access.clear()
+            obj.read_access.clear()
+        response = self.client.get(self.get_url(), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['object_list'].count())
 
     def test_object(self):
-        self.empl.read_access.add(self.division)
         response = self.client.get(self.get_url(), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['object_list'].count(), 1)    # FIXME
-
-    def test_empty_object(self):
-        self.empl.read_access.clear()
-        self.empl.full_access.clear()
-        response = self.client.get(self.get_url(), follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['object_list'].count(), 0)
+        self.assertEqual(
+            response.context['object_list'].count(),
+            self.model_access.objects.all().count()
+        )
