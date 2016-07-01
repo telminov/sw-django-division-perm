d4e8b9460a2c1a116b23047724205d2b07bc1be1
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-01 21:26:25 +0300

    #9 - готово

--- division_perm/tests/base.py
+++ division_perm/tests/base.py
@@ -9,19 +9,25 @@ from division_perm import models
 
 class BaseTest(TestCase):
     view_path = None
+    fixtures = ['perm_func']
 
-    def setUp(self): # todo: fixture, как-то хитро загружается
+    def setUp(self):
         self.user = User.objects.create_user(username='tester', email='tester@soft-way.biz', password='123')
-        self.empl = models.Employee.objects.create(user=self.user, last_name='test', first_name='test', middle_name='test')
-        func_read = models.Func.objects.create(code=consts.SYS_READ_FUNC, name='test_read', level=0)
-        models.Func.objects.create(code=consts.SYS_EDIT_FUNC, name='test_edit', level=0)
-        self.division = models.Division.objects.create(name='TechSupport')
-        self.division.employees.add(self.empl)
-        role_read = models.Role.objects.create(name='Manager', code=func_read.code, level=9, division=self.division)
-        self.empl.roles.add(role_read)
-        self.empl.full_access.add(self.division)
-        self.division.full_access.add(self.division)
+        other_user = User.objects.create_user(username='other_user', email='other_user@soft-way.biz', password='123')
+        empl = models.Employee.objects.create(user=self.user, last_name='other_test', first_name='other_test', middle_name='other_test')
+        other_empl = models.Employee.objects.create(user=other_user, last_name='test', first_name='test', middle_name='test')
+        func_edit = models.Func.objects.get(code=consts.SYS_EDIT_FUNC)
+        division = models.Division.objects.create(name='TechSupport')
+        division.employees.add(empl)
+        role_edit = models.Role.objects.create(name='Manager', code=func_edit.code, level=9, division=division)
+        empl.roles.add(role_edit)
+        empl.full_access.add(division)
+        other_empl.full_access.add(division)
+        division.full_access.add(division)
         self.client.login(username=self.user.username, password='123')
+
+        self.generate_data()
 
     def get_url(self):
         url = reverse(self.view_path)
+        return url
