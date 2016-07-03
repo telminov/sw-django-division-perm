62ec6055c78a7570e49bde8d3ccb5e9c174f8d40
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-03 12:39:24 +0300

    #5 - рефакторинг в связи с появлением base

--- division_perm/tests/test_func.py
+++ division_perm/tests/test_func.py
@@ -1,44 +1,25 @@
 # coding: utf-8
 from .. import consts
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 
-from django.test import TestCase
-from division_perm import models
-from division_perm.tests.helpers import ListTestMixin, DetailTestMixin
+from division_perm.tests.base import BaseTest
+from division_perm.tests.helpers import *
 
 
-class BaseFuncTest(TestCase):
-    view_path = None
-
-    def setUp(self):
-        self.user = User.objects.create_user(username='tester', email='tester@soft-way.biz', password='123')
-        self.empl = models.Employee.objects.create(user=self.user, last_name='test', first_name='test', middle_name='test')
-        func_edit = models.Func.objects.create(code=consts.SYS_EDIT_FUNC, name='test_edit', level=5)
-        self.func_read = models.Func.objects.create(code=consts.SYS_READ_FUNC, name='test_read', level=0)
-        self.division = models.Division.objects.create(name='Tech Support')
-        self.division.employees.add(self.empl)
-        role_read = models.Role.objects.create(name='manger', code=self.func_read.code, level=9, division=self.division)
-        self.empl.roles.add(role_read)
-        self.client.login(username=self.user.username, password='123')
-
-    def get_url(self):
-        url = reverse(self.view_path)
-        return url
-
-
-class ListFuncTest(BaseFuncTest, ListTestMixin):
+class ListFuncTest(FuncAccessTestMixin, ListTestMixin, BaseTest):
     view_path = 'perm_func_list'
     success_url = reverse('perm_func_list') + '?sort=code'
     model_access = models.Func
+    func_code = consts.SYS_READ_FUNC
 
 
-class DetailFuncTest(BaseFuncTest, DetailTestMixin):
+class DetailFuncTest(FuncAccessTestMixin, DetailTestMixin, BaseTest):
     view_path = 'perm_func_detail'
+    func_code = consts.SYS_READ_FUNC
 
     def get_url(self):
-        url = reverse(self.view_path, args=[self.func_read.id])
+        url = reverse(self.view_path, args=[self.get_instance().id])
         return url
 
     def get_instance(self):
-        return self.func_read
+        func_read = models.Func.objects.get(code=consts.SYS_READ_FUNC)
+        return func_read
