3b6c7f52fbd0efda8c0a758c09c04577c9671b6a
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-03 12:01:59 +0300

    #2 - переработка в связи с дополнениями миксинов

--- division_perm/tests/test_employee.py
+++ division_perm/tests/test_employee.py
@@ -1,13 +1,10 @@
 # coding: utf-8
 from .. import consts
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 
-from division_perm import models
 from division_perm.tests.base import BaseTest
 from division_perm.tests.helpers import *
 
-class BaseEmployee(BaseTest): # todo: порядок классов!!
+class BaseEmployee(BaseTest):
     view_path = None
 
     def generate_data(self):
@@ -31,6 +28,22 @@ class BaseEmployee(BaseTest): # todo: порядок классов!!
         return p
 
 
+    def get_ident_emp_param(self):
+        p = self.get_params()
+
+        return {
+            'user__username': p['username'],
+            'last_name': p['last_name'],
+            'middle_name': p['middle_name'],
+            'first_name': p['first_name'],
+            'divisions__id__in': [p['divisions']],
+            'full_access__id__in': [p['full_access']],
+            'read_access__id__in': [p['full_access']],
+            'user__is_active': p['is_active'],
+            'can_external': p['can_external'],
+        }
+
+
 class EmployeeListTest(FuncAccessTestMixin, ListTestMixin, ListAccessTestMixin, BaseEmployee):
     view_path = 'perm_employee_list'
     success_url = reverse('perm_employee_list') + '?sort=last_name'
@@ -38,7 +51,7 @@ class EmployeeListTest(FuncAccessTestMixin, ListTestMixin, ListAccessTestMixin, 
     func_code = consts.SYS_READ_FUNC
 
 
-class EmployeeDetailTest(BaseEmployee, DetailTestMixin, ReadAccessTestMixin):
+class EmployeeDetailTest(DetailTestMixin, ReadAccessTestMixin, BaseEmployee):
     view_path = 'perm_employee_detail'
     model_access = models.Employee
 
@@ -57,43 +70,26 @@ class EmployeeCreateTest(CreateTestMixin, BaseEmployee):
     exclude_fields = ['password1', 'password2', 'username', 'is_active']
 
     def get_ident_param(self):
-        p = self.get_params()
-        return {
-            'user__username': p['username'],
-            'last_name': p['last_name'],
-            'first_name': p['first_name'],
-            'divisions__id__in': [p['divisions']],
-            'full_access__id__in': [p['full_access']],
-        }
+        return self.get_ident_emp_param()
 
-class EmployeeUpdateTest(BaseEmployee):
+
+class EmployeeUpdateTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployee):
     view_path = 'perm_employee_update'
+    model = models.Employee
+    success_path = 'perm_employee_detail'
+
+    def get_instance(self):
+        return self.empl
 
     def get_url(self):
-        url = reverse(self.view_path, args=[self.empl.id])
+        url = reverse(self.view_path, args=[self.get_instance().id])
         return url
 
-    def test_200(self):
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 200)
-
-    def test_update(self):
-        p = self.get_params()
-        response = self.client.post(self.get_url(), p, follow=True)
-        self.assertEqual(response.status_code, 200)
-        update_empl = models.Employee.objects.get(id=self.empl.id)
-        self.assertEqual(response.redirect_chain[0], ('/perm/employee/%s/' % update_empl.id, 302))
-        self.assertEqual(p['last_name'], update_empl.last_name)
-        self.assertEqual(p['first_name'], update_empl.first_name)
-        self.assertEqual(p['middle_name'], update_empl.middle_name)
-        self.assertIn(p['divisions'], update_empl.divisions.all().values_list('id', flat=True))
-        self.assertIn(p['full_access'], update_empl.full_access.all().values_list('id', flat=True))
-        self.assertIn(p['read_access'], update_empl.read_access.all().values_list('id', flat=True))
-        self.assertEqual(p['is_active'], update_empl.user.is_active)
-        self.assertEqual(p['can_external'], update_empl.can_external)
+    def get_ident_param(self):
+       return self.get_ident_emp_param()
 
 
-class EmployeeDeleteTest(BaseEmployee, DeleteTestMixin):
+class EmployeeDeleteTest(DeleteTestMixin, BaseEmployee):
     view_path = 'perm_employee_delete'
     model = models.Employee
 
@@ -108,50 +104,53 @@ class EmployeeDeleteTest(BaseEmployee, DeleteTestMixin):
         return self.other_empl
 
 
-class EmployeePasswordChangeTest(BaseEmployee):
+class EmployeePasswordChangeTest(UpdateTestMixin, ModifyAccessTestMixin,  BaseEmployee):
     view_path = 'perm_employee_password_change'
+    model = models.Employee
 
     def get_url(self):
         url = reverse(self.view_path, args=[self.empl.id])
         return url
 
-    def test_200(self):
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 200)
+    def get_instance(self):
+        return self.user.employee
 
-    def test_change(self):
-        new_password = u'new_password'
-        old_password = self.empl.user.password
-        p = {
-            'password1': new_password,
-            'password2': new_password,
+    def get_params(self):
+        return {
+            'password1': 'new_password',
+            'password2': 'new_password',
         }
-        response = self.client.post(self.get_url(), p, follow=True)
+
+    def test_update(self):
+        old_password = self.get_instance().user.password
+
+        response = self.client.post(self.get_url(), self.get_params(), follow=True)
         self.assertEqual(response.redirect_chain[0], ('..', 302))
-        update_empl = models.Employee.objects.get(id=self.empl.id)
+        update_empl = models.Employee.objects.get(id=self.get_instance().id)
         self.assertNotEqual(update_empl.user.password, old_password)
 
 
-class EmployeeRolesTest(BaseEmployee):
+class EmployeeRolesTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployee):
     view_path = 'perm_employee_roles'
+    model = models.Employee
+    success_path = 'perm_employee_detail'
+
+    def get_instance(self):
+        return self.user.employee
 
     def get_url(self):
         url = reverse(self.view_path, args=[self.empl.id])
         return url
 
-    def test_200(self):
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 200)
-
-    def test_edit_roles(self):
-        role = models.Role.objects.get(code=consts.SYS_EDIT_FUNC)
-        role.division = self.division
-        role.save()
-
-        p = {
+    def get_params(self):
+        return {
             'user': self.empl.user,
-            'roles': [role.id],
+            'roles': [self.get_instance().id],
         }
-        response = self.client.post(self.get_url(), p, follow=True)
-        self.assertEqual(response.redirect_chain[0], ('/perm/employee/%s/' % self.empl.id, 302))
-        self.assertIn(role, self.empl.roles.all())
+
+    def get_ident_param(self):
+        p = self.get_params()
+
+        return {
+            'roles__id__in': p['roles'],
+        }
