3f9ea2f4ac2493152e92d389505f826596850fe1
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-05 22:02:16 +0300

    #5 - добавление factories.Func

--- division_perm/factories.py
+++ division_perm/factories.py
@@ -9,3 +9,10 @@ class Division(factory.django.DjangoModelFactory):
         model = models.Division
 
     name = factory.fuzzy.FuzzyText(length=15, prefix='test_division_')
+
+
+class Func(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Func
+
+    name = factory.fuzzy.FuzzyText(length=15, prefix='test_func_')
