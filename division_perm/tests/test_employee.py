e47430ad75c2bb4b1e6b2cb082cf78966dabe08e
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-05 23:26:42 +0300

    #2 - рефакторинг в процессе

--- division_perm/tests/test_employee.py
+++ division_perm/tests/test_employee.py
@@ -1,150 +1,146 @@
 # coding: utf-8
-from .. import consts
 
-from division_perm.tests.base import BaseTest
-from division_perm.tests.helpers import *
-
-class BaseEmployeeTest(BaseTest):
-    view_path = None
-
-    def get_params(self):
-        division = models.Division.objects.all()[0]
-
-        p = {
-            'username': 'ivanov',
-            'password1': 't1234567',
-            'password2': 't1234567',
-            'last_name': 'Ivanov',
-            'first_name': 'Ivan',
-            'middle_name': 'Ivanovich',
-            'divisions': division.id,
-            'full_access': division.id,
-            'read_access': division.id,
-            'is_active': True,
-            'can_external': False,
-        }
-        return p
+from ..views import employee
+from ..tests.base import BaseTest
+from ..tests.helpers import *
+from .. import factories
 
 
-    def get_ident_emp_param(self):
-        p = self.get_params()
 
-        return {
-            'user__username': p['username'],
-            'last_name': p['last_name'],
-            'middle_name': p['middle_name'],
-            'first_name': p['first_name'],
-            'divisions__id__in': [p['divisions']],
-            'full_access__id__in': [p['full_access']],
-            'read_access__id__in': [p['full_access']],
-            'user__is_active': p['is_active'],
-            'can_external': p['can_external'],
-        }
+# class BaseEmployeeTest(BaseTest):
+#     view_path = None
+#
+#     def get_params(self):
+#         division = models.Division.objects.all()[0]
+#
+#         p = {
+#             'username': 'ivanov',
+#             'password1': 't1234567',
+#             'password2': 't1234567',
+#             'last_name': 'Ivanov',
+#             'first_name': 'Ivan',
+#             'middle_name': 'Ivanovich',
+#             'divisions': division.id,
+#             'full_access': division.id,
+#             'read_access': division.id,
+#             'is_active': True,
+#             'can_external': False,
+#         }
+#         return p
+#
+#
+#     def get_ident_emp_param(self):
+#         p = self.get_params()
+#
+#         return {
+#             'user__username': p['username'],
+#             'last_name': p['last_name'],
+#             'middle_name': p['middle_name'],
+#             'first_name': p['first_name'],
+#             'divisions__id__in': [p['divisions']],
+#             'full_access__id__in': [p['full_access']],
+#             'read_access__id__in': [p['full_access']],
+#             'user__is_active': p['is_active'],
+#             'can_external': p['can_external'],
+#         }
 
 
-class EmployeeListTest(FuncAccessTestMixin, ListTestMixin, ListAccessTestMixin, BaseEmployeeTest):
+class EmployeeListTest(SortTestMixin, ListAccessTestMixin, FuncAccessTestMixin, LoginRequiredTestMixin, BaseTest):
     view_path = 'perm_employee_list'
-    success_url = reverse('perm_employee_list') + '?sort=last_name'
-    model_access = models.Employee
-    func_code = consts.SYS_READ_FUNC
+    view_class = employee.List
+    factory_class = factories.Employee
 
 
-class EmployeeDetailTest(DetailTestMixin, ReadAccessTestMixin, BaseEmployeeTest):
+class EmployeeDetailTest(ReadAccessTestMixin, LoginRequiredTestMixin, BaseTest):
     view_path = 'perm_employee_detail'
-    model_access = models.Employee
-
-    def get_instance(self):
-        return self.user.employee
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
+    view_class = employee.Detail
+    factory_class = factories.Employee
 
 
-class EmployeeCreateTest(CreateTestMixin, BaseEmployeeTest):
-    view_path = 'perm_employee_create'
-    model = models.Employee
-    success_path = 'perm_employee_detail'
-
-    def get_ident_param(self):
-        return self.get_ident_emp_param()
-
-
-class EmployeeUpdateTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployeeTest):
-    view_path = 'perm_employee_update'
-    model = models.Employee
-    success_path = 'perm_employee_detail'
-
-    def get_instance(self):
-        return self.user.employee
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
-
-    def get_ident_param(self):
-       return self.get_ident_emp_param()
-
-
-class EmployeeDeleteTest(DeleteTestMixin, BaseEmployeeTest):
-    view_path = 'perm_employee_delete'
-    model = models.Employee
-
-    def get_instance(self):
-        return models.Employee.objects.exclude(id=self.user.employee.id)[0]
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
-
-
-class EmployeePasswordChangeTest(UpdateTestMixin, ModifyAccessTestMixin,  BaseEmployeeTest):
-    view_path = 'perm_employee_password_change'
-    model = models.Employee
-
-    def get_instance(self):
-        return self.user.employee
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
-
-    def get_params(self):
-        return {
-            'password1': 'new_password',
-            'password2': 'new_password',
-        }
-
-    def test_update(self):
-        old_password = self.get_instance().user.password
-
-        response = self.client.post(self.get_url(), self.get_params(), follow=True)
-        self.assertEqual(response.redirect_chain[0], ('..', 302))
-        update_empl = models.Employee.objects.get(id=self.get_instance().id)
-        self.assertNotEqual(update_empl.user.password, old_password)
-
-
-class EmployeeRolesTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployeeTest):
-    view_path = 'perm_employee_roles'
-    model = models.Employee
-    success_path = 'perm_employee_detail'
-
-    def get_instance(self):
-        return self.user.employee
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
-
-    def get_params(self):
-        return {
-            'user': self.get_instance().user,
-            'roles': [self.get_instance().id],
-        }
-
-    def get_ident_param(self):
-        p = self.get_params()
-
-        return {
-            'roles__id__in': p['roles'],
-        }
+# class EmployeeCreateTest(CreateTestMixin, BaseEmployeeTest):
+#     view_path = 'perm_employee_create'
+#     model = models.Employee
+#     success_path = 'perm_employee_detail'
+#
+#     def get_ident_param(self):
+#         return self.get_ident_emp_param()
+#
+#
+# class EmployeeUpdateTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployeeTest):
+#     view_path = 'perm_employee_update'
+#     model = models.Employee
+#     success_path = 'perm_employee_detail'
+#
+#     def get_instance(self):
+#         return self.user.employee
+#
+#     def get_url(self):
+#         url = reverse(self.view_path, args=[self.get_instance().id])
+#         return url
+#
+#     def get_ident_param(self):
+#        return self.get_ident_emp_param()
+#
+#
+# class EmployeeDeleteTest(DeleteTestMixin, BaseEmployeeTest):
+#     view_path = 'perm_employee_delete'
+#     model = models.Employee
+#
+#     def get_instance(self):
+#         return models.Employee.objects.exclude(id=self.user.employee.id)[0]
+#
+#     def get_url(self):
+#         url = reverse(self.view_path, args=[self.get_instance().id])
+#         return url
+#
+#
+# class EmployeePasswordChangeTest(UpdateTestMixin, ModifyAccessTestMixin,  BaseEmployeeTest):
+#     view_path = 'perm_employee_password_change'
+#     model = models.Employee
+#
+#     def get_instance(self):
+#         return self.user.employee
+#
+#     def get_url(self):
+#         url = reverse(self.view_path, args=[self.get_instance().id])
+#         return url
+#
+#     def get_params(self):
+#         return {
+#             'password1': 'new_password',
+#             'password2': 'new_password',
+#         }
+#
+#     def test_update(self):
+#         old_password = self.get_instance().user.password
+#
+#         response = self.client.post(self.get_url(), self.get_params(), follow=True)
+#         self.assertEqual(response.redirect_chain[0], ('..', 302))
+#         update_empl = models.Employee.objects.get(id=self.get_instance().id)
+#         self.assertNotEqual(update_empl.user.password, old_password)
+#
+#
+# class EmployeeRolesTest(UpdateTestMixin, ModifyAccessTestMixin, BaseEmployeeTest):
+#     view_path = 'perm_employee_roles'
+#     model = models.Employee
+#     success_path = 'perm_employee_detail'
+#
+#     def get_instance(self):
+#         return self.user.employee
+#
+#     def get_url(self):
+#         url = reverse(self.view_path, args=[self.get_instance().id])
+#         return url
+#
+#     def get_params(self):
+#         return {
+#             'user': self.get_instance().user,
+#             'roles': [self.get_instance().id],
+#         }
+#
+#     def get_ident_param(self):
+#         p = self.get_params()
+#
+#         return {
+#             'roles__id__in': p['roles'],
+#         }
