fddbec7653c53e9a4c1f029a9c0400a873679ebe
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-05 22:01:24 +0300

    #5 - рефакторинг

--- division_perm/tests/test_func.py
+++ division_perm/tests/test_func.py
@@ -1,25 +1,17 @@
 # coding: utf-8
-from .. import consts
+from ..views import func
+from ..tests.base import BaseTest
+from ..tests.helpers import *
+from .. import factories
 
-from division_perm.tests.base import BaseTest
-from division_perm.tests.helpers import *
 
-
-class ListFuncTest(FuncAccessTestMixin, ListTestMixin, BaseTest):
+class ListFuncTest(SortTestMixin, FuncAccessTestMixin, LoginRequiredTestMixin, BaseTest):
     view_path = 'perm_func_list'
-    success_url = reverse('perm_func_list') + '?sort=code'
-    model_access = models.Func
-    func_code = consts.SYS_READ_FUNC
+    view_class = func.List
+    factory_class = factories.Func
 
 
-class DetailFuncTest(FuncAccessTestMixin, DetailTestMixin, BaseTest):
+class DetailFuncTest(SingleObjectTestMixin, FuncAccessTestMixin, LoginRequiredTestMixin, BaseTest):
     view_path = 'perm_func_detail'
-    func_code = consts.SYS_READ_FUNC
-
-    def get_url(self):
-        url = reverse(self.view_path, args=[self.get_instance().id])
-        return url
-
-    def get_instance(self):
-        func_read = models.Func.objects.get(code=consts.SYS_READ_FUNC)
-        return func_read
+    view_class = func.Detail
+    factory_class = factories.Func
