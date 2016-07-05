3de5318b4a3075b555963e2b651cfd7ecab00309
vbif2004@yandex.ru <vbif2004@yandex.ru>
2016-07-05 23:30:42 +0300

    #3 - добавление factories.Emploee

--- division_perm/factories.py
+++ division_perm/factories.py
@@ -2,6 +2,7 @@
 import factory
 import factory.fuzzy
 from . import models
+from django.contrib.auth.models import User
 
 
 class Division(factory.django.DjangoModelFactory):
@@ -16,3 +17,34 @@ class Func(factory.django.DjangoModelFactory):
         model = models.Func
 
     name = factory.fuzzy.FuzzyText(length=15, prefix='test_func_')
+
+
+class Role(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Role
+
+    division = factory.SubFactory(Division)
+    code = factory.fuzzy.FuzzyText(length=15, prefix='test_role_code_')
+    name = factory.fuzzy.FuzzyText(length=15, prefix='test_role_name_')
+
+
+class User(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.User
+
+    username = factory.fuzzy.FuzzyText(length=15, prefix='test_user_')
+
+
+class Employee(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Employee
+
+    user = factory.SubFactory(User)
+    last_name = factory.fuzzy.FuzzyText(length=15, prefix='test_employee_')
+
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        role = Role(level=9)
+        employee = super(Employee, cls)._prepare(create, **kwargs)
+        employee.roles.add(role)
+        return employee
